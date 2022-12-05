d9ddc504b7cdf5085b9bf559532638004b44443d
Colin Brough <colinbrough73@gmail.com>
2022-12-05 21:55:16 +0000

    Library update

--- Misc.c
+++ Misc.c
@@ -3,9 +3,12 @@
  *	Misc.c		Miscellaneous utility routines
  *
  *----------------------------------------------------------------------
- * $Id: Misc.c,v 1.1 2000/05/09 20:24:00 cmb Exp $
+ * $Id: Misc.c,v 1.2 2003/04/22 23:06:33 cmb Exp $
  *
  * $Log: Misc.c,v $
+ * Revision 1.2  2003/04/22 23:06:33  cmb
+ * Updated.
+ *
  * Revision 1.1  2000/05/09 20:24:00  cmb
  * Initial revision
  *
@@ -31,4 +34,24 @@ int strlastcmp(char *s1, char *s2)
         s1 = s1 + (l1 - l2);
     }
     return(strcmp(s1, s2));
+}
+
+/*----------------------------------------------------------------------
+ * ReturnTime	Routine to return the current date and time
+ *----------------------------------------------------------------------*/
+
+void ReturnTime(int *year, int *month, int *day, int *hour, int *min, int *sec)
+{
+    time_t t;
+    struct tm *T;
+
+    time( &t );
+    T = localtime(&t);
+    
+    *second = T->tm_sec;
+    *min    = T->tm_min;
+    *hour   = T->tm_hour;
+    *day    = T->tm_mday;
+    *month  = T->tm_mon + 1;
+    *year   = T->tm_year + 1900;
 }
